136b06af3ffaf9dde52773345ffc04747b109490
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-29 21:08:25 +0500

    add entities for post, comment, like and enum/post

--- ContentService/internal/entity/post.go
+++ ContentService/internal/entity/post.go
@@ -1,1 +1,23 @@
 package entity
+
+import "time"
+
+type PostType string
+
+const (
+	PostTypeLegalInfo PostType = "LEGAL_INFO"
+	PostTypeGuide     PostType = "GUIDE"
+)
+
+type Post struct {
+	ID            string
+	Title         string
+	Content       string
+	Type          PostType
+	AuthorID      string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Published     bool
+	LikesCount    int32
+	DislikesCount int32
+}
