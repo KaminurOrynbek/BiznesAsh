4dc4bd58a4a5cd1dcb89e2b69b2af6245988cdcf
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-29 21:59:16 +0500

    created post usecase interface for ContentService

--- ContentService/internal/usecase/interface/post.go
+++ ContentService/internal/usecase/interface/post.go
@@ -1,1 +1,15 @@
-package usecase
+package _interface
+
+import (
+	"context"
+	"github.com/KaminurOrynbek/BiznesAsh/internal/entity"
+)
+
+type PostUsecase interface {
+	CreatePost(ctx context.Context, post *entity.Post) error
+	UpdatePost(ctx context.Context, post *entity.Post) error
+	DeletePost(ctx context.Context, id string) error
+	GetPost(ctx context.Context, id string) (*entity.Post, error)
+	ListPosts(ctx context.Context) ([]*entity.Post, error)
+	SearchPosts(ctx context.Context, keyword string) ([]*entity.Post, error)
+}
