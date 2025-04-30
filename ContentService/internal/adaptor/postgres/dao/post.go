412991cf2e5c02ec064d141852290267ae5fd44e
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-30 13:25:51 +0500

    added PostDAO

--- ContentService/internal/adaptor/postgres/dao/post.go
+++ ContentService/internal/adaptor/postgres/dao/post.go
@@ -1,1 +1,23 @@
 package dao
+
+import (
+	"github.com/KaminurOrynbek/BiznesAsh/internal/entity/enum"
+	"time"
+)
+
+type Post struct {
+	ID            string        `db:"id"`
+	Title         string        `db:"title"`
+	Content       string        `db:"content"`
+	Type          enum.PostType `db:"type"`
+	AuthorID      string        `db:"author_id"`
+	CreatedAt     time.Time     `db:"created_at"`
+	UpdatedAt     time.Time     `db:"updated_at"`
+	Published     bool          `db:"published"`
+	LikesCount    int32         `db:"likes_count"`
+	DislikesCount int32         `db:"dislikes_count"`
+}
+
+func (Post) TableName() string {
+	return "post"
+}
