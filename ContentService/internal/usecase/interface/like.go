f82e540f591d0081b75886580d7d57481cfe3186
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-30 12:50:06 +0500

    fixed usecase/interface/like.go passing a full entity.Like object

--- ContentService/internal/usecase/interface/like.go
+++ ContentService/internal/usecase/interface/like.go
@@ -1,8 +1,11 @@
 package _interface
 
-import "context"
+import (
+	"context"
+	"github.com/KaminurOrynbek/BiznesAsh/internal/entity"
+)
 
 type LikeUsecase interface {
-	LikePost(ctx context.Context, postID, userID string) error
-	DislikePost(ctx context.Context, postID, userID string) error
+	LikePost(ctx context.Context, like *entity.Like) error
+	DislikePost(ctx context.Context, like *entity.Like) error
 }
