10b4ba4cf9a654b717f394c937cc1b63eb949514
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-30 13:26:15 +0500

    added LikeDAO

--- ContentService/internal/adaptor/postgres/dao/like.go
+++ ContentService/internal/adaptor/postgres/dao/like.go
@@ -1,1 +1,15 @@
 package dao
+
+import "time"
+
+type Like struct {
+	ID        string    `db:"id"`
+	PostID    string    `db:"post_id"`
+	UserID    string    `db:"user_id"`
+	IsLike    bool      `db:"is_like"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+func (Like) TableName() string {
+	return "like"
+}
