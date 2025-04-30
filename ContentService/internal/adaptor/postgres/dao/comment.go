55c499d36b2c7f5925b7c7d658f0b89da36acf05
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-04-30 13:25:27 +0500

    added CommentDAO

---
+++ ContentService/internal/adaptor/postgres/dao/comment.go
@@ -0,0 +1,16 @@
+package dao
+
+import "time"
+
+type Comment struct {
+	ID        string    `db:"id"`
+	PostID    string    `db:"post_id"`
+	AuthorID  string    `db:"author_id"`
+	Content   string    `db:"content"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+func (Comment) TableName() string {
+	return "comment"
+}
