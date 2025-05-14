79ffe43330160cdaf8a3dbddd9712858e1c973ae
kaminurorinbek <kaminurorinbek@gmail.com>
2025-05-14 12:06:33 +0500

    hot fix: userusecase in main

--- UserService/cmd/user/main.go
+++ UserService/cmd/user/main.go
@@ -1,56 +1,68 @@
 package main
 
 import (
-	pb "github.com/KaminurOrynbek/BiznesAsh/UserService/auto-proto/user"
-	nats "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/nats"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/nats/publisher"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/postgres/dao"
-	natscfg "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/configs/nats"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/configs/posgres"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/delivery/grpc"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/middleware"
-	usecase "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/usecase/Impl"
-	"github.com/KaminurOrynbek/BiznesAsh/UserService/pkg/queue"
+	"log"
+	"net"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	gogrpc "google.golang.org/grpc"
-	"log"
-	"net"
+
+	pb "github.com/KaminurOrynbek/BiznesAsh/UserService/auto-proto/user"
+	nats "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/nats"
+	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/nats/publisher"
+	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/adapter/postgres/dao"
+	"github.com/KaminurOrynbek/BiznesAsh/UserService/pkg/queue"
+	natscfg "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/configs/nats"
+	posgres "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/configs/posgres"
+	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/delivery/grpc"
+	"github.com/KaminurOrynbek/BiznesAsh/UserService/internal/middleware"
+	usecase "github.com/KaminurOrynbek/BiznesAsh/UserService/internal/usecase/Impl"
 )
 
 func main() {
+	// Load .env if exists
 	err := godotenv.Load()
 	if err != nil {
 		log.Printf("Warning: Could not load .env file: %v", err)
 	}
 
+	// Load Postgres config
 	cfg := posgres.LoadConfig()
 
+	// Connect to Postgres
 	db, err := sqlx.Connect("postgres", cfg.GetDBURL())
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 	defer db.Close()
 
+	// Init user repo
 	userRepo := dao.NewUserDAO(db)
 
+	// NATS setup
 	natsConfig := natscfg.LoadConfig()
-	natsConn := nats.NewConnection(natsConfig)
-	natsQueue := queue.NewNATSQueue(natsConn)
-	defer natsConn.Close()
+	rawConn := nats.NewConnection(natsConfig)
+	defer rawConn.Close()
 
-	userPublisher := publisher.NewUserPublisher(natsQueue)
+	// Wrap NATS into queue-compatible interface
+	msgQueue := queue.NewNATSQueue(rawConn)
 
+	// Create publisher and usecase
+	userPublisher := publisher.NewUserPublisher(msgQueue)
 	userUsecase := usecase.NewUserUsecase(userRepo, userPublisher)
-	userServer := grpc.NewUserServer(userUsecase)
 
+	// Create gRPC server
+	userServer := grpc.NewUserServer(userUsecase)
 	grpcServer := gogrpc.NewServer(
 		gogrpc.UnaryInterceptor(middleware.AuthInterceptor),
 	)
 
+	// Register gRPC service
 	pb.RegisterUserServiceServer(grpcServer, userServer)
 
+	// Start listener
 	listener, err := net.Listen("tcp", ":"+cfg.GRPCPort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
